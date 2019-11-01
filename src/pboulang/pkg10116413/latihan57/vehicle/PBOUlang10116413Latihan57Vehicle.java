ca41a8190ca02583ba2b026523b229c68e29eb82
Harry Apriadi <harryapriadi1@gmail.com>
2019-11-01 19:43:57 +0700

    PBOUlang-10116413-Latihan57-Vehicle

---
+++ src/pboulang/pkg10116413/latihan57/vehicle/PBOUlang10116413Latihan57Vehicle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10116413.latihan57.vehicle;
+
+/**
+ *
+ * @author Lenovo
+ * NAMA         : HARRY APRIADI
+ * KELAS        : PBOULANG
+ * NIM          : 10116413
+ * DESKRIPSI    : Membuat Program Info Vehicle
+ */
+public class PBOUlang10116413Latihan57Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bicycle bicycle = new Bicycle();
+        Skateboard skateboard = new Skateboard();
+    }
+
+}
